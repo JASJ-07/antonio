994094e4be198df580e4ff639bd1b5915934ab55
Antonio <antoniosar.43@gmail.com>
2018-02-17 00:15:36 -0500

    layout

---
+++ app/src/main/java/com/example/antonio/miprimerlayout/RelativelayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.antonio.miprimerlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativelayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relativelayout);
+        R.id.txtNombre;
+    }
+}
